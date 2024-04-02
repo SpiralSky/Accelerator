a4265c41b54615c60cafa01b9899131fe8eabc1d
spiralsky <shianastrophysics@gmail.com>
2024-04-02 16:18:44 +0800

    Undid Test Commit

--- src/main/java/com/spiralsky/accelerator/Init/ItemInit.java
+++ src/main/java/com/spiralsky/accelerator/Init/ItemInit.java
@@ -7,6 +7,5 @@ import net.minecraftforge.registries.ForgeRegistries;
 import static com.spiralsky.accelerator.Accelerator.MODID;
 
 public class ItemInit {
-    //TEST
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 }
